a5cf76ad6383b637369558af72e2760a12a997ab
Wormsandmaggots <Sobrado@o2.pl>
2021-02-26 10:46:55 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/git_true.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/git_true.dir/main.c.obj"
+  "git_true.exe"
+  "git_true.exe.manifest"
+  "git_true.lib"
+  "git_true.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/git_true.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
