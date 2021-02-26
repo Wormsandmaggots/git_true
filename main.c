9fdbc69e99c67e5be2a0eee99e2e09e9e76eee93
Wormsandmaggots <Sobrado@o2.pl>
2021-02-26 10:56:29 +0100

    01 zmiana metody liczenia

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
+int policz(int,int);
 int main() {
     printf("Hello, World!\n");
+    printf("w pierwszym branchu 2+3=%d",policz(2,3));
     return 0;
+}
+int policz(int a,int b)
+{
+    return a + b;
 }
